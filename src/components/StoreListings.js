29acecf082ae38019a969cb0877db988c0d2397d
Your Name <you@example.com>
2022-12-29 21:42:15 -0800

    Still figuring out the bug

--- src/components/StoreListings.js
+++ src/components/StoreListings.js
@@ -7,13 +7,13 @@ import Col from 'react-bootstrap/Col'
 
 export default function StoreListings() {
     return (
-        <div style={{ margin: "auto" }}>
+        <div style={{ margin: "auto"}}>
             <h2 style={{ fontSize: "36px", lineHeight: "50px", marginTop: "20px", textAlign: "center", fontWeight: "normal" }}>Browse stores in Houston</h2>
-            <Container style={{ marginTop: "50px", textAlign: "center" }}>
+            <Container style={{ marginTop: "50px", textAlign: "center"}}>
                 <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
                     <Row>
-                        <Col xs=".5" >
-                            <Image style={{ height: "55px", width: "50px", border: "1px solid #E8E9EB" }} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png" roundedCircle />
+                        <Col xs=".9" >
+                            <Image style={{ height: "55px", width: "50px", border: "1px solid #E8E9EB", marginLeft:"20px"}} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png" roundedCircle />
                         </Col>
                         <Col style={{textAlign: "left"}}>
                             <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
