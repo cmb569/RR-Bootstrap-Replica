1c69db060167bcea1bb675577818cac210d0c77d
Your Name <you@example.com>
2022-12-30 20:51:46 -0800

    changing codes

--- src/components/StoreListings.js
+++ src/components/StoreListings.js
@@ -12,7 +12,7 @@ export default function StoreListings() {
             <Container style={{ marginTop: "50px", textAlign: "center"}}>
                 <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
                     <Row>
-                        <Col xs=".9" >
+                        <Col xs=".5" >
                             <Image style={{ height: "55px", width: "50px", border: "1px solid #E8E9EB", marginLeft:"20px"}} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png" roundedCircle />
                         </Col>
                         <Col style={{textAlign: "left"}}>
@@ -23,7 +23,7 @@ export default function StoreListings() {
                     <Col sm="1"></Col>
                     <Row >
                         <Col xs=".5">
-                            <Image style={{ height: "50px", width: "50px", border: "1px solid #E8E9EB" }} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/279/0d43b5d2-9d07-4a30-a0ee-0e16d5922411.png" roundedCircle />
+                            <Image style={{ height: "50px", width: "50px", border: "1px solid #E8E9EB", margin:"3000px 0 0 0"}} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/279/0d43b5d2-9d07-4a30-a0ee-0e16d5922411.png" roundedCircle />
                         </Col>
                         <Col style={{textAlign: "left"}}>
                             <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
